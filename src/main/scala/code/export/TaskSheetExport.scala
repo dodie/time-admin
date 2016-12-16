0fb2d12c0a02faee44906bcfbd96bd27a9554b79
suliatis <attila.suli@ponte.hu>
2016-12-16 12:21:47 +0100

    Refine xlsx styles.

--- src/main/scala/code/export/TaskSheetExport.scala
+++ src/main/scala/code/export/TaskSheetExport.scala
@@ -2,18 +2,14 @@ package code.export
 
 import code.model.User
 import code.service.ReportService
-import code.util.TaskSheetUtils
 import code.util.TaskSheetUtils._
-import com.norbitltd.spoiwo.model.enums.CellHorizontalAlignment.Center
-import com.norbitltd.spoiwo.model.{CellStyle, _}
-import com.norbitltd.spoiwo.model.enums.CellFill
-import net.liftweb.http.S
-import org.joda.time.ReadablePartial
 import com.github.nscala_time.time.Imports._
-import com.norbitltd.spoiwo.model.enums.CellBorderStyle.{Medium, Thin}
-import net.liftweb.common.Box
+import com.norbitltd.spoiwo.model._
 import com.norbitltd.spoiwo.natures.xlsx.Model2XlsxConversions.XlsxSheet
+import net.liftweb.common.Box
+import net.liftweb.http.S
 import org.apache.poi.xssf.usermodel.XSSFWorkbook
+import org.joda.time.ReadablePartial
 
 object TaskSheetExport {
 
@@ -34,43 +30,70 @@ object TaskSheetExport {
         Row(main(fullTitle)) ::
           Row(taskTitle :: (ds.map(d => header(d.toString)) :+ sumTitle)) :: {
           ts.map { t =>
-            Row(Cell(t.name) :: (ds.map { d =>
-              value(duration(taskSheet, d, t).minutes).withStyle(if (isWeekend(interval, d)) weekend else bold)
-            } :+ value(sumByTasks(taskSheet)(t).minutes)))
+            Row(left(t.name) :: (ds.map { d =>
+              value(duration(taskSheet, d, t).minutes).withDefaultStyle(weekend(interval, d))
+            } :+ summary(sumByTasks(taskSheet)(t).minutes)))
           } :+
             Row(sumFooter :: (ds.map { d => footer(sumByDates(taskSheet)(d).minutes) } :+ footer(sum(taskSheet).minutes)))
         },
       columns = autoSizedColumns(range(columnWith)),
       mergedRegions = List(CellRange(0 -> 0, columnWith)),
-      paneAction = FreezePane(1, 2)
+      paneAction = FreezePane(1, 2),
+      style = styles.default
     ).convertAsXlsx()
 
     (xlsx, s"tasksheet_${fullTitle.toLowerCase.replace(" ", "")}.xlsx")
   }
 
-  def taskTitle: Cell = header(S.?("export.tasksheet.project_identifier"))
+  def taskTitle: Cell = Cell(value = S.?("export.tasksheet.project_identifier"), style = styles.header.withHorizontalAlignment(styles.left.horizontalAlignment.get))
 
   def sumTitle: Cell = header(S.?("export.tasksheet.sum"))
 
-  def sumFooter: Cell = footer(S.?("export.tasksheet.sum"))
+  def sumFooter: Cell = Cell(value = S.?("export.tasksheet.sum"), style = styles.footer.withHorizontalAlignment(styles.left.horizontalAlignment.get))
 
-  def main[T: CellValueType](t: T): Cell = Cell(value = t, style = bold.withHorizontalAlignment(Center))
+  def main[T: CellValueType](t: T): Cell = Cell(value = t, style = styles.main)
 
-  def header[T: CellValueType](t: T): Cell = Cell(value = t, style = bold.withBorders(CellBorders().withBottomStyle(Medium)))
+  def header[T: CellValueType](t: T): Cell = Cell(value = t, style = styles.header)
 
-  def value[T: CellValueType](t: T): Cell = Cell(value = t, style = bold)
+  def left[T: CellValueType](t: T): Cell = Cell(value = t, style = styles.left)
 
-  def footer[T: CellValueType](t: T): Cell = Cell(value = t, style = bold.withBorders(CellBorders().withTopStyle(Thin)))
+  def value[T: CellValueType](t: T): Cell = Cell(value = t, style = styles.default)
+
+  def summary[T: CellValueType](t: T): Cell = Cell(value = t, style = styles.summary)
+
+  def footer[T: CellValueType](t: T): Cell = Cell(value = t, style = styles.footer)
 
   def autoSizedColumns(range: Range): List[Column] = (for (i <- range) yield Column(index = i, autoSized = true)).toList
 
   def range(p: (Int, Int)): Range = p match { case (start, end) => start to end }
 
-  def isWeekend(i: Interval, d: ReadablePartial): Boolean =
-    Some(d).filter(hasDayFieldType).flatMap(mapToDateTime(i, _)).exists(TaskSheetUtils.isWeekend)
+  def weekend(i: Interval, d: ReadablePartial): Option[CellStyle] =
+    Some(d) filter hasDayFieldType flatMap (mapToDateTime(i, _)) filter isWeekend map (_ => styles.weekend)
 
-  private lazy val weekend = CellStyle(font = Font(bold = true), fillPattern = CellFill.Solid, fillForegroundColor = Color(221, 221, 221))
+  object styles {
+    import com.norbitltd.spoiwo.model.Color.{LightGrey, LightYellow}
+    import com.norbitltd.spoiwo.model.enums.CellFill.Solid
+    import com.norbitltd.spoiwo.model.enums.CellHorizontalAlignment.{Center, Left, Right}
+    import com.norbitltd.spoiwo.model.enums.CellBorderStyle.{Medium, None, Thin}
+    import com.norbitltd.spoiwo.model.{CellStyle, _}
 
-  private lazy val bold: CellStyle = CellStyle(font = Font(bold = true))
+    lazy val default = CellStyle(
+      font = Font(bold = true),
+      horizontalAlignment = Right,
+      borders = CellBorders(None)
+    )
+
+    lazy val main = default.withHorizontalAlignment(Center)
+
+    lazy val header = default.withBorders(CellBorders().withBottomStyle(Medium))
+
+    lazy val left = default.withHorizontalAlignment(Left)
+
+    lazy val footer = default.withBorders(CellBorders().withTopStyle(Thin))
+
+    lazy val weekend = default.withFillPattern(Solid).withFillForegroundColor(LightGrey)
+
+    lazy val summary = default.withFillPattern(Solid).withFillForegroundColor(LightYellow)
+  }
 
 }
