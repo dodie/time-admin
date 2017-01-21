4cd6b7bf3405e6853754eafbba94b92d85b55894
Attila Süli <suli.zakar.attila@gmail.com>
2017-01-21 21:03:30 +0100

    Refactor test data initialization.

--- src/it/scala/code/service/ReportServiceTest.scala
+++ src/it/scala/code/service/ReportServiceTest.scala
@@ -3,20 +3,17 @@ package code.service
 import code.model.{Project, Task, TaskItem, User}
 import code.test.utils.BaseSuite
 import code.util.TaskSheetUtils.{dates, sum, tasks}
+import net.liftweb.common.{Box, Empty, Full}
 import net.liftweb.mapper.By
-import org.joda.time.{DateTime, LocalDate, YearMonth}
+import com.github.nscala_time.time.Imports._
 
 class ReportServiceTest extends BaseSuite {
 
-  describe("Task sheet data for default user and single day") {
-    lazy val ts = ReportService.taskSheetData(
-      new LocalDate(2016, 1, 30).toInterval(),
-      d => d,
-      User.find(By(User.email, "default@tar.hu"))
-    )
+  describe("Report task sheet data for default user and single day") {
+    lazy val ts = ReportService.taskSheetData(date(2016, 1, 30).toInterval(), identity, defaultUser())
 
     it("the summary should be 8 hours") {
-      sum(ts).getStandardHours should be (8L)
+      sum(ts) shouldBe 8.hours.toDuration
     }
 
     it("should contain all tasks for the interval") {
@@ -24,19 +21,15 @@ class ReportServiceTest extends BaseSuite {
     }
 
     it("should contain all dates for the interval") {
-      dates(ts) should contain theSameElementsAs List(new LocalDate(2016, 1, 30))
+      dates(ts) should contain theSameElementsAs List(date(2016, 1, 30))
     }
   }
 
-  describe("Task sheet data for default user and overflowed day") {
-    lazy val ts = ReportService.taskSheetData(
-      new LocalDate(2016, 1, 31).toInterval(),
-      d => d,
-      User.find(By(User.email, "default@tar.hu"))
-    )
+  describe("Report task sheet data for default user and overflowed day") {
+    lazy val ts = ReportService.taskSheetData(date(2016, 1, 31).toInterval(), identity, defaultUser())
 
     it("the summary should be 8 and a half hours") {
-      sum(ts).getStandardMinutes should be (8L * 60L + 30L)
+      sum(ts) shouldBe (8.hours + 30.minutes).toDuration
     }
 
     it("should contain all tasks for the interval") {
@@ -44,19 +37,15 @@ class ReportServiceTest extends BaseSuite {
     }
 
     it("should contain all dates for the interval") {
-      dates(ts) should contain theSameElementsAs List(new LocalDate(2016, 1, 31))
+      dates(ts) should contain theSameElementsAs List(date(2016, 1, 31))
     }
   }
 
-  describe("Task sheet data for default user and day with slipped tasks") {
-    lazy val ts = ReportService.taskSheetData(
-      new LocalDate(2016, 2, 1).toInterval(),
-      d => d,
-      User.find(By(User.email, "default@tar.hu"))
-    )
+  describe("Report task sheet data for default user and day with slipped tasks") {
+    lazy val ts = ReportService.taskSheetData(date(2016, 2, 1).toInterval(), identity, defaultUser())
 
     it("the summary should be 8 and a half hours") {
-      sum(ts).getStandardMinutes should be (8L * 60L + 30L)
+      sum(ts) shouldBe (8.hours + 30.minutes).toDuration
     }
 
     it("should contain all tasks for the interval") {
@@ -64,19 +53,15 @@ class ReportServiceTest extends BaseSuite {
     }
 
     it("should contain all dates for the interval") {
-      dates(ts) should contain theSameElementsAs List(new LocalDate(2016, 2, 1))
+      dates(ts) should contain theSameElementsAs List(date(2016, 2, 1))
     }
   }
 
-  describe("Task sheet data for default user and single month") {
-    lazy val ts = ReportService.taskSheetData(
-      new YearMonth(2016, 1).toInterval(),
-      d => d,
-      User.find(By(User.email, "default@tar.hu"))
-    )
+  describe("Report task sheet data for default user and single month") {
+    lazy val ts = ReportService.taskSheetData(new YearMonth(2016, 1).toInterval(), identity, defaultUser())
 
     it("the summary should be 16 and a half hours") {
-      sum(ts).getStandardMinutes should be (16L * 60L + 30L)
+      sum(ts) shouldBe (16.hours + 30.minutes).toDuration
     }
 
     it("should contain all tasks for the interval") {
@@ -84,53 +69,57 @@ class ReportServiceTest extends BaseSuite {
     }
 
     it("should contain all dates for the interval") {
-      dates(ts) should contain theSameElementsAs Stream.iterate(new LocalDate(2016, 1, 1))(_.plusDays(1)).take(31)
+      dates(ts) should contain theSameElementsAs Stream.iterate(date(2016, 1, 1))(_.plusDays(1)).take(31)
     }
   }
 
+  def defaultUser(): Box[User] = User.find(By(User.email, "default@mail.com"))
+
   given {
-    val u1 = User.create
-      .firstName("DEFAULT")
-      .lastName("DEFAULT")
-      .email("default@tar.hu")
-      .password("abc123")
-      .validated(true)
-      .superUser(true)
-      .saveMe()
+    val u1 :: u2 :: _ = givenUsers("default", "other") map (_.saveMe())
 
-    val p1 = Project.create.name("p1").active(true).saveMe()
-    val p11 = Project.create.name("p11").parent(p1).active(true).saveMe()
-    val p12 = Project.create.name("p12").parent(p1).active(true).saveMe()
-    val p2 = Project.create.name("p2").active(true).saveMe()
+    val p1 :: p11 :: p12 :: _ = givenProjects("p1", "p11", "p12") map (_.saveMe())
+    val p2 :: _ = givenProjects("p2") map (_.saveMe())
 
+    val t1 :: t2 :: t3 :: t4 :: t5 :: t6 :: t7 :: _ = givenTasks("t1" -> p1, "t2" -> p1, "t3" -> p11, "t4" -> p12, "t5" -> p2, "t6" -> p2, "t7" -> p2) map (_.saveMe()) map (Full(_))
 
-    val t1 = Task.create.name("t1").parent(p1).active(true).saveMe()
-    val t2 = Task.create.name("t2").parent(p1).active(true).saveMe()
-    val t3 = Task.create.name("t3").parent(p11).active(true).saveMe()
-    val t4 = Task.create.name("t4").parent(p12).active(true).saveMe()
-    val t5 = Task.create.name("t5").parent(p2).active(true).saveMe()
-    val t6 = Task.create.name("t6").parent(p2).active(true).saveMe()
-    val t7 = Task.create.name("t7").parent(p2).active(true).saveMe()
+    val pause = Empty
 
-    TaskItem.create.user(u1).task(t1).start(new DateTime(2016, 1, 30, 8, 30).getMillis).save()
-    TaskItem.create.user(u1).task(t2).start(new DateTime(2016, 1, 30, 10, 25).getMillis).save()
-    TaskItem.create.user(u1).task(-1L).start(new DateTime(2016, 1, 30, 12, 0).getMillis).save()
-    TaskItem.create.user(u1).task(t3).start(new DateTime(2016, 1, 30, 12, 30).getMillis).save()
-    TaskItem.create.user(u1).task(-1L).start(new DateTime(2016, 1, 30, 17, 0).getMillis).save()
-
-    TaskItem.create.user(u1).task(t1).start(new DateTime(2016, 1, 31, 8, 30).getMillis).save()
-    TaskItem.create.user(u1).task(t2).start(new DateTime(2016, 1, 31, 10, 25).getMillis).save()
-    TaskItem.create.user(u1).task(-1L).start(new DateTime(2016, 1, 31, 12, 0).getMillis).save()
-    TaskItem.create.user(u1).task(t3).start(new DateTime(2016, 1, 31, 12, 30).getMillis).save()
-    TaskItem.create.user(u1).task(-1L).start(new DateTime(2016, 1, 31, 17, 0).getMillis).save()
-
-    TaskItem.create.user(u1).task(t7).start(new DateTime(2016, 1, 31, 23, 30).getMillis).save()
-    TaskItem.create.user(u1).task(-1L).start(new DateTime(2016, 2, 1, 0, 30).getMillis).save()
-
-    TaskItem.create.user(u1).task(t4).start(new DateTime(2016, 2, 1, 8, 30).getMillis).save()
-    TaskItem.create.user(u1).task(t5).start(new DateTime(2016, 2, 1, 10, 25).getMillis).save()
-    TaskItem.create.user(u1).task(-1L).start(new DateTime(2016, 2, 1, 12, 0).getMillis).save()
-    TaskItem.create.user(u1).task(t6).start(new DateTime(2016, 2, 1, 12, 30).getMillis).save()
-    TaskItem.create.user(u1).task(-1L).start(new DateTime(2016, 2, 1, 17, 0).getMillis).save()
+    givenTaskItems(u1, date(2016, 1, 30),
+      t1 -> time(8, 30), t2 -> time(10, 25), pause -> time(12, 0), t3 -> time(12, 30), pause -> time(17, 0)
+    ) :::
+    givenTaskItems(u1, date(2016, 1, 31),
+      t1 -> time(8, 30), t2 -> time(10, 25), pause -> time(12, 0), t3 -> time(12, 30), pause -> time(17, 0), t7 -> time(23, 30)
+    ) :::
+    givenTaskItems(u1, date(2016, 2, 1),
+      pause -> time(0, 30), t4 -> time(8, 30), t5 -> time(10, 25), pause -> time(12, 0), t6 -> time(12, 30), pause -> time(17, 0)
+    ) :::
+    givenTaskItems(u2, date(2016, 1, 30),
+      t1 -> time(8, 30), t2 -> time(10, 25), pause -> time(12, 0), t3 -> time(12, 30), pause -> time(17, 0)
+    ) :::
+    givenTaskItems(u2, date(2016, 1, 31),
+      t1 -> time(8, 30), t2 -> time(10, 25), pause -> time(12, 0), t3 -> time(12, 30), pause -> time(17, 0), t7 -> time(23, 30)
+    ) :::
+    givenTaskItems(u2, date(2016, 2, 1),
+      pause -> time(0, 30), t4 -> time(8, 30), t5 -> time(10, 25), pause -> time(12, 0), t6 -> time(12, 30), pause -> time(17, 0)
+    ) foreach (_.save())
   }
+
+  def givenUsers(names: String*): List[User] =
+    names map { n => User.create.firstName(n).lastName(n).email(n + "@mail.com").password("abc123").validated(true).superUser(true) } toList
+
+  def givenProjects(name: String, children: String*): List[Project] =
+    List(Project.create.name(name).active(true)) flatMap { p =>
+      p :: { children map (n => Project.create.name(n).parent(p).active(true)) toList }
+    }
+
+  def givenTasks(ts: (String, Project)*): List[Task] =
+    ts map { case (t, p) => Task.create.name(t).parent(p).active(true) } toList
+
+  def givenTaskItems(u: User, ld: LocalDate, tis: (Box[Task], LocalTime)*): List[TaskItem] =
+    tis map { case (t, lt) => TaskItem.create.user(u).task(t).start(ld.toDateTime(lt).getMillis) } toList
+
+  def date(year: Integer, monthOfYear: Integer, dayOfMonth: Integer): LocalDate = new LocalDate(year, monthOfYear, dayOfMonth)
+
+  def time(hourOfDay: Integer, minuteOfHour: Integer): LocalTime = new LocalTime(hourOfDay, minuteOfHour)
 }
