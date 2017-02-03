c87caebf9cb6b3024001c7361ae7da3038743dd3
David Csakvari <david.csakvari@ponte.hu>
2017-02-03 11:59:33 +0100

    Remove Project model, merging it into Task.

--- src/it/scala/code/service/ProjectServiceTest.scala
+++ src/it/scala/code/service/ProjectServiceTest.scala
@@ -7,50 +7,50 @@ import net.liftweb.mapper.By
 class ProjectServiceTest extends BaseSuite {
   describe("Project Service") {
     it("Display name of the top level project") {
-      assert(ProjectService.getDisplayName(projectByName("top")) == "top")
+      assert(TaskService.getDisplayName(projectByName("top")) == "top")
     }
 
     it("Display name of the middle level project") {
-      assert(ProjectService.getDisplayName(projectByName("middle")) == "top-middle")
+      assert(TaskService.getDisplayName(projectByName("middle")) == "top-middle")
     }
 
     it("Display name of the bottom level project") {
-      assert(ProjectService.getDisplayName(projectByName("bottom")) == "top-middle-bottom")
+      assert(TaskService.getDisplayName(projectByName("bottom")) == "top-middle-bottom")
     }
 
     it("Move bottom project to any parent project") {
-      ProjectService.move(projectByName("bottom"), projectByName("any project"))
+      TaskService.move(projectByName("bottom"), projectByName("any project"))
 
-      assert(ProjectService.getDisplayName(projectByName("bottom")) == "top-any project-bottom")
+      assert(TaskService.getDisplayName(projectByName("bottom")) == "top-any project-bottom")
     }
 
     it("Move bottom project to root") {
-      ProjectService.moveToRoot(projectByName("bottom"))
+      TaskService.moveToRoot(projectByName("bottom"))
 
-      assert(ProjectService.getDisplayName(projectByName("bottom")) == "bottom")
+      assert(TaskService.getDisplayName(projectByName("bottom")) == "bottom")
     }
 
     it("The top level project is not empty") {
-      assert(!ProjectService.isEmpty(projectByName("top")))
+      assert(!TaskService.isEmpty(projectByName("top")))
     }
 
     it("The bottom level project is empty") {
-      assert(ProjectService.isEmpty(projectByName("bottom")))
+      assert(TaskService.isEmpty(projectByName("bottom")))
     }
 
     it("Delete a bottom level project") {
-      assert(ProjectService.delete(projectByName("bottom")))
+      assert(TaskService.delete(projectByName("bottom")))
     }
 
     it("Delete an active top level project") {
-      ProjectService.delete(projectByName("top"))
+      TaskService.delete(projectByName("top"))
       assert(!projectByName("top").active.get)
     }
 
     it("Try to delete an inactivated top level project") {
       val me = projectByName("top").active(false).saveMe()
       intercept[IllegalArgumentException] {
-        ProjectService.delete(me)
+        TaskService.delete(me)
       }
     }
   }
