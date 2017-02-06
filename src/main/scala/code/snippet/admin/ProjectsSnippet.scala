144624b5046b991e7744ac280cd32657a146d86f
David Csakvari <david.csakvari@ponte.hu>
2017-02-06 18:10:12 +0100

    Remove Project model, merging it into Task.

--- src/main/scala/code/snippet/admin/ProjectsSnippet.scala
+++ src/main/scala/code/snippet/admin/ProjectsSnippet.scala
@@ -18,7 +18,7 @@ import scala.util.Sorting
 import scala.xml.NodeSeq
 
 /**
- * Project snippet.
+ * Task administration snippet.
  * @author David Csakvari
  */
 class ProjectsSnippet {
@@ -38,7 +38,7 @@ class ProjectsSnippet {
   }
 
   def addRoot: CssSel = {
-    ".add-root [onclick]" #> SHtml.ajaxInvoke(addRootEditor).toJsCmd
+    ".add-root [onclick]" #> SHtml.ajaxInvoke(() => addChild(Empty)).toJsCmd
   }
 
   def moveToRoot: CssSel = {
@@ -53,98 +53,9 @@ class ProjectsSnippet {
     "a [onclick]" #> SHtml.ajaxInvoke(submit _).toJsCmd
   }
 
-  def projects(in: NodeSeq): NodeSeq = {
-    projectTemplate ++ taskTemplate
+  def tasks(in: NodeSeq): NodeSeq = {
+    taskTemplate
   }
-
-  def projectList(in: NodeSeq): NodeSeq = {
-    val parentId: Long = {
-      try {
-        (in \ "div" \ "div").filter(_.attribute("class").get.text == "parentId").text.toLong
-      } catch {
-        case _: Exception => -1
-      }
-    }
-
-    val data = (if (parentId == -1) {
-      if (showInactiveTasks.get) {
-        Task.findAll(By(Task.selectable, false)).filter(_.parent.isEmpty)
-      } else {
-        Task.findAll(By(Task.active, true), By(Task.selectable, false)).filter(_.parent.isEmpty)
-      }
-    } else {
-      if (showInactiveTasks.get) {
-        Task.findAll(By(Task.selectable, false), By(Task.parent, Task.find(By(Task.id, parentId)).openOrThrowException("Parent must be defined!")))
-      } else {
-        Task.findAll(By(Task.selectable, false), By(Task.parent, Task.find(By(Task.id, parentId)).openOrThrowException("Parent must be defined!")), By(Task.active, true))
-      }
-    }).toArray
-
-    if (parentId == -1 && data.isEmpty) {
-      <lift:embed what="no_data"/>
-    } else {
-      Sorting.quickSort(data)(new Ordering[Task] {
-        def compare(x: Task, y: Task): Int = {
-          collator.compare(x.name.get, y.name.get)
-        }
-      })
-      data.toSeq.flatMap(task => renderProject(task)(in))
-    }
-  }
-
-  private def renderProject(task: Task) = {
-    val displayName =
-      if (task.active.get)
-        task.name.get
-      else
-        task.name.get + " (" + S.?("projects.inactive") + ")"
-
-    var rootClass = "project"
-    if (selectedTask.get == task) rootClass += " selected"
-
-    var innerClass = "projectName"
-    if (!task.active.get) innerClass += " inactive"
-
-    // TODO Remove snippet based recursion.
-    val subsCssSel:CssSel = ".parentId *" #> task.id.toString
-
-    ".name" #> displayName &
-    ".root-class [class]" #> rootClass &
-    ".inner-class [class]" #> innerClass &
-    ".edit [onclick]" #> SHtml.ajaxInvoke(() => editor(task)).toJsCmd &
-    ".add-subtask [onclick]" #> SHtml.ajaxInvoke(() => addChild(task)).toJsCmd &
-    ".delete [onclick]" #> SHtml.ajaxInvoke(() => deleteTask(task)).toJsCmd &
-    ".select [onclick]" #> SHtml.ajaxInvoke(() => selectTask(task)).toJsCmd &
-    ".moveto [onclick]" #> SHtml.ajaxInvoke(() => moveTo(task)).toJsCmd &
-    ".merge [onclick]" #> SHtml.ajaxInvoke(() => mergeTask(task)).toJsCmd &
-    ".subprojects *" #> subsCssSel(projectTemplate) &
-    ".subtasks *" #> subsCssSel(taskTemplate)
-  }
-
-  private val projectTemplate: NodeSeq =
-    <div class="lift:projectsSnippet.projectList">
-      <div class="root-class">
-        <div class="parentId" style="display:none;"></div>
-        <span class="inner-class">
-          <span class="dropdown">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-              <span class="name"></span>
-              <span class="caret"></span>
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="edit"><lift:loc>projects.edit</lift:loc></a></li>
-              <li><a class="add-subtask"><lift:loc>projects.add_task</lift:loc></a></li>
-              <li><a class="delete"><lift:loc>projects.delete</lift:loc></a></li>
-              <li><a class="select"><lift:loc>projects.select</lift:loc></a></li>
-              <li><a class="moveto"><lift:loc>projects.moveto</lift:loc></a></li>
-              <li><a class="merge"><lift:loc>projects.mergeinto</lift:loc></a></li>
-            </ul>
-          </span>
-        </span>
-        <div class="subprojects"></div>
-        <div class="subtasks"></div>
-      </div>
-    </div>
 
   private val taskTemplate: NodeSeq =
     <div class="lift:projectsSnippet.taskList">
@@ -166,7 +77,6 @@ class ProjectsSnippet {
             </ul>
           </span>
         </span>
-        <div class="subprojects"></div>
         <div class="subtasks"></div>
       </div>
     </div>
@@ -184,15 +94,15 @@ class ProjectsSnippet {
       val parent = Task.find(By(Task.id, parentId))
 
       if (!showInactiveTasks.get) {
-        Task.findAll(By(Task.parent, parent), By(Task.active, true), By(Task.selectable, true))
+        Task.findAll(By(Task.parent, parent), By(Task.active, true))
       } else {
-        Task.findAll(By(Task.parent, parent), By(Task.selectable, true))
+        Task.findAll(By(Task.parent, parent))
       }
     } else {
       if (!showInactiveTasks.get) {
-        Task.findAll(By(Task.parent, Empty), By(Task.active, true), By(Task.selectable, true))
+        Task.findAll(By(Task.parent, Empty), By(Task.active, true))
       } else {
-        Task.findAll(By(Task.parent, Empty), By(Task.selectable, true))
+        Task.findAll(By(Task.parent, Empty))
       }
     }
 
@@ -234,10 +144,9 @@ class ProjectsSnippet {
     ".edit [onclick]" #> SHtml.ajaxInvoke(() => editor(task)).toJsCmd &
     ".delete [onclick]" #> SHtml.ajaxInvoke(() => deleteTask(task)).toJsCmd &
     ".select [onclick]" #> SHtml.ajaxInvoke(() => selectTask(task)).toJsCmd &
-    ".add-subtask [onclick]" #> SHtml.ajaxInvoke(() => addChild(task)).toJsCmd &
+    ".add-subtask [onclick]" #> SHtml.ajaxInvoke(() => addChild(Full(task))).toJsCmd &
     ".merge [onclick]" #> SHtml.ajaxInvoke(() => mergeTask(task)).toJsCmd &
     ".moveto [onclick]" #> SHtml.ajaxInvoke(() => moveTo(task)).toJsCmd &
-    ".subprojects *" #> subsCssSel(projectTemplate) &
     ".subtasks *" #> subsCssSel(taskTemplate)
   }
 
@@ -319,59 +228,11 @@ class ProjectsSnippet {
     openDialog
   }
 
-  private def addRootEditor(): JsCmd = {
-    object name extends TransientRequestVar("")
-    object description extends TransientRequestVar("")
-    object color extends TransientRequestVar("")
-
-    def submit: JsCmd = {
-      Task.create
-        .name(name.get)
-        .description(description.get)
-        .color(color.get)
-        .active(true)
-        .specifiable(true)
-        .save
-
-      rerenderTree &
-      closeDialog
-    }
-
-    SetHtml("inject",
-      (
-        ".fields *" #>
-          (
-            renderProperty(
-              ".name *" #> S.?("projects.popup.name") &
-              ".field" #> SHtml.textElem(name, "class" -> "form-control")) ++
-            renderProperty(
-              ".name *" #> S.?("projects.popup.description") &
-              ".field" #> SHtml.textElem(description, "class" -> "form-control")) ++
-            renderProperty(
-              ".name *" #> S.?("projects.popup.color") &
-              ".field" #> SHtml.textElem(color, "class" -> "form-control", "type" -> "color"))
-            ) &
-        ".title *" #> S.?("projects.new_project") &
-        ".submit-button" #> SHtml.ajaxSubmit(S.?("button.save"), submit _, "class" -> "btn btn-primary") &
-        ".close-button" #> SHtml.ajaxSubmit(S.?("button.close"), closeDialog _, "class" -> "btn btn-default")
-
-      )(editorTemplate)
-    ) &
-    openDialog
-  }
-
-  def renderProperty(cssSel: CssSel): NodeSeq = {
+  private def renderProperty(cssSel: CssSel): NodeSeq = {
     cssSel(editorPropertyTemplate)
   }
 
-  val editorPropertyTemplate: NodeSeq =
-    <div class="form-group">
-      <label class="name"></label>
-      <input class="field"/>
-    </div>
-
-
-  private def addChild(parent: Task): JsCmd = {
+  private def addChild(parent: Box[Task]): JsCmd = {
     object name extends TransientRequestVar("")
     object description extends TransientRequestVar("")
     object specifiable extends TransientRequestVar(true)
@@ -478,12 +339,11 @@ class ProjectsSnippet {
     openDialog
   }
 
-
   def closeDialog: JsCmd = JsRaw("$('.modal').modal('hide')").cmd
 
   def openDialog: JsCmd = JsRaw("$('.modal').modal()").cmd
 
-  def rerenderTree: JsCmd = SetHtml("project-tree", projects(NodeSeq.Empty))
+  def rerenderTree: JsCmd = SetHtml("project-tree", tasks(NodeSeq.Empty))
 
   val editorTemplate: NodeSeq =
     <div class="modal fade" data-backdrop="static" data-keyboard="false">
@@ -501,6 +361,12 @@ class ProjectsSnippet {
           </div>
         </form>
       </div>
+    </div>
+
+  val editorPropertyTemplate: NodeSeq =
+    <div class="form-group">
+      <label class="name"></label>
+      <input class="field"/>
     </div>
 
 }
