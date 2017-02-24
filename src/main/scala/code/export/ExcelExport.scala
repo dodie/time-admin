9fa17ffb16debfd0a545310739cf5bf35a011abc
suliatis <attila.suli@ponte.hu>
2017-02-24 18:14:44 +0100

    Fix NPE at time sheet export.

--- src/main/scala/code/export/ExcelExport.scala
+++ src/main/scala/code/export/ExcelExport.scala
@@ -66,7 +66,7 @@ object ExcelExport {
       findCell("{ta_month}") foreach { cell => cell.setCellValue(monthText) }
       for (i <- 1 to 31) {
         val data = if (dates.size >= i) dates(i - 1) else (null, null, null, null)
-        findCell("{ta_date_" + i + "}") foreach { cell => cell.setCellValue(data._1.toString) }
+        findCell("{ta_date_" + i + "}") foreach { cell => cell.setCellValue(if (data._1 != null) data._1.toString else null) }
         findCell("{ta_arrive_" + i + "}") foreach { cell => cell.setCellValue(data._2) }
         findCell("{ta_leave_" + i + "}") foreach { cell => cell.setCellValue(data._3) }
       }
