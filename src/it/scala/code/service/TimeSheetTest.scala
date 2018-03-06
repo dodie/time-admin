6458df1e01f25441b50459d2b6db453698274a58
David Csakvari <david.csakvari@ponte.hu>
2018-03-06 13:32:45 +0100

    Fix tests

--- src/it/scala/code/service/TimeSheetTest.scala
+++ src/it/scala/code/service/TimeSheetTest.scala
@@ -13,7 +13,7 @@ class TimeSheetTest extends BaseSuite {
   describe("Time sheet data for any month") {
     lazy val ts = {
       User.currentUser map (_.subtractBreaks(true)) foreach (_.save())
-      ReportService.getTimesheetData(IntervalQuery(yearMonth(2016, 1).toInterval))
+      ReportService.getTimesheetData(IntervalQuery(yearMonth(2016, 1).toInterval), User.currentUser)
     }
 
     it("should have log entries subtracted by the breaks") { withS(Empty, defaultUser()) {
@@ -28,7 +28,7 @@ class TimeSheetTest extends BaseSuite {
   describe("Time sheet data for any month with disabled break subtraction") {
     lazy val ts = {
       User.currentUser map (_.subtractBreaks(false)) foreach (_.save())
-      ReportService.getTimesheetData(IntervalQuery(yearMonth(2016, 1).toInterval))
+      ReportService.getTimesheetData(IntervalQuery(yearMonth(2016, 1).toInterval), User.currentUser)
     }
 
     it("should have log entries with breaks") { withS(Empty, defaultUser()) {
